fbdef5b9cca164049c3af29ffa1750a9c0c154c7
udaysinghkanwar <udaykanwar007@gmail.com>
2026-01-05 16:49:35 -0500

    fixed navbar on small screens

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -55,7 +55,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border rounded-full  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white[0.2] bg-black-100",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-4 py-2 sm:px-10 sm:py-5 items-center justify-center space-x-4 border-white/20 bg-black-100",
           className
         )}
       >
@@ -69,8 +69,10 @@ export const FloatingNav = ({
               "relative text-neutral-50 items-center flex space-x-1 hover:text-neutral-300"
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            {navItem.icon ? (
+              <span className="block sm:hidden">{navItem.icon}</span>
+            ) : null}
+            <span className="text-xs sm:text-sm">{navItem.name}</span>
           </a>
         ))}
       </motion.div>
